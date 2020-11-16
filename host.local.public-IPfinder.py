bf44609effc817e3fbae148b4666946ca969b47a
Mohan <74539498+N90t@users.noreply.github.com>
2020-11-16 19:04:36 +0530

    Update host.local.public-IPfinder.py

--- host.local.public-IPfinder.py
+++ host.local.public-IPfinder.py
@@ -1,7 +1,13 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import socket
-from requests import get
+from requests import get  #install request module
 
 hostname =  socket.gethostname
+localIP  = socket.gethostbyname(hostname)
+publicIP = get('http://ifconfig.me')
 
+#Here, we're using f-strings is to make string interpolation simpler.It's same way that you would with str.format()
+print(f"Hostname: {hostname}")
+print(f"Local_IP: {localIP}")
+print(f'Public_IP: {publicIP}")
