d36bdf32fe46c18df42632320326db25d1380dfe
Mohan <74539498+N90t@users.noreply.github.com>
2020-11-16 22:33:46 +0530

    Update host.local.public-IPfinder.py

--- host.local.public-IPfinder.py
+++ host.local.public-IPfinder.py
@@ -11,7 +11,7 @@ hostname =  socket.gethostname
 localIP  = socket.gethostbyname(hostname)
 publicIP = get('http://ifconfig.me').text
 
-#Here, we're using f-strings is to make string interpolation simpler.It's same way that you would with str.format()
+#Here, we're using f-strings to make string interpolation simpler.It's same way that you would with str.format()
 print(f"Hostname: {hostname}")
 print(f"Local_IP: {localIP}")
 print(f'Public_IP: {publicIP}")
